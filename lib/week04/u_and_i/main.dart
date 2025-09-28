47ec83341c636ac878ee748aac78c5cc4c2ab8e6
thengelsec <ssuk83158315@gmail.com>
2025-09-28 23:15:42 +0900

    feat: Apply default theme and sunflower font

--- lib/week04/u_and_i/main.dart
+++ lib/week04/u_and_i/main.dart
@@ -4,7 +4,10 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: HomeScreen(),
+      theme: ThemeData(   // 테마를 지정할 수 있는 클래스
+        fontFamily: 'sunflower',    // 기본 글씨체
+        
+      )
     ),
   );
 }
