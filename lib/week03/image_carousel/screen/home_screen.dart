ff9fc39587ca0aa85c751f52285fc61f4e28114a
thengelsec <ssuk83158315@gmail.com>
2025-09-28 21:07:10 +0900

    feat: Implement basic PageView for image carousel

--- lib/week03/image_carousel/screen/home_screen.dart
+++ lib/week03/image_carousel/screen/home_screen.dart
@@ -6,7 +6,12 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+      body: PageView( // PageView 추가
+        children: [1, 2, 3, 4, 5] // 샘플 리스트 생성
+            .map(   // 위젯으로 매핑
+              
+            )
+      )
     );
   }
 }
