8ad4169a80c994e882888ed255ffbfbdbbf41e40
thengelsec <ssuk83158315@gmail.com>
2025-09-28 21:08:10 +0900

    feat: Implement basic image carousel using PageView

--- lib/week03/image_carousel/screen/home_screen.dart
+++ lib/week03/image_carousel/screen/home_screen.dart
@@ -9,7 +9,9 @@ class HomeScreen extends StatelessWidget {
       body: PageView( // PageView 추가
         children: [1, 2, 3, 4, 5] // 샘플 리스트 생성
             .map(   // 위젯으로 매핑
-        (number) => Image.asset('asset/img/image_$number.jpeg'),
+        (number) => Image.asset(
+          'asset/img/image_$number.jpeg'
+          ),
             )
             .toList(),
       ),
