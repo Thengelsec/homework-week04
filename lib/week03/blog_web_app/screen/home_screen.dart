4cbee3bd95a04ab13b6367064e31d62779688ecd
thengelsec <ssuk83158315@gmail.com>
2025-09-28 20:54:40 +0900

    feat: Implement basic home screen UI with AppBar

--- lib/week03/blog_web_app/screen/home_screen.dart
+++ lib/week03/blog_web_app/screen/home_screen.dart
@@ -2,5 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
-  
+  WebViewController webViewController = WebViewController();
+
+  HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // 앱바 위젯 추가
+      appBar: AppBar(
+
+        // 배경색 지정
+        backgroundColor: Colors.orange,
+
+        // 앱 타이틀 설정
+        title: Text('Code Factory'),
+
+        // 가운데 정렬
+        centerTitle: true,
+      ),
+      body: Text('Home Screen'),
+    );
+  }
 }
